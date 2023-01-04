f6b4b025cd2df841a7013f585a65b55c823443bd
Artem <aholevko@maxilect.com>
2023-01-04 14:37:23 +0300

    berore and after junit

---
+++ src/test/java/junit/FirstJUTest.java
@@ -0,0 +1,43 @@
+package junit;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Browsers.FIREFOX;
+
+public class FirstJUTest {
+
+@BeforeAll
+static void beforeAll() {
+    Configuration.browser = FIREFOX;
+    System.out.println("all");
+}
+    @AfterAll
+    static void afterAll() {
+        Configuration.browser = FIREFOX;
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+
+        System.out.println("dsfsdfsdfdsf");
+    }
+    @AfterEach
+    void afterEach() {
+
+        System.out.println("sdfdsfgfdgdfg");
+
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("gfhfg");
+
+    }
+    @Test
+    void secondTest() {
+        System.out.println("gfeeeehfg");
+
+    }
+}
